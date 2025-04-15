bd65b566ce8d026724d45364d69ca6ad08a634ac
Lakshin Kumar <lakshin.kumar.lk@gmail.com>
2025-04-15 17:14:54 +0530

    tailwind-setup

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",  // if you have an index.html file
+    "./src/**/*.{js,ts,jsx,tsx}",  // if you're using React or similar
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
